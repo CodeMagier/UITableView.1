9a97063d0a5fdfcaa0c6dafe88fe61d227c9e292
CodeMagier <Ios23.git@gmail.com>
2024-01-25 16:01:02 +0600

    Initial Commit

---
+++ UITableView.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableView.1
+//
+//  Created by 1 on 25/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
